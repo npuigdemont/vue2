5f319f6aa731a0327ca0edf1020f70e0d2586f6b
Nuri <72916917+npuigdemont@users.noreply.github.com>
2022-04-08 12:26:02 +0200

    exercici1

---
+++ components/Escena.js
@@ -0,0 +1,7 @@
+Vue.component('Escena', {
+    template: `
+    <div>
+    <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+    </div>
+    `
+})
---
+++ components/Home.js
@@ -0,0 +1,8 @@
+Vue.component('Home', {
+    template: 
+    `
+    <div class="p-5">
+        <Escena></Escena>
+    </div>
+    `
+})
